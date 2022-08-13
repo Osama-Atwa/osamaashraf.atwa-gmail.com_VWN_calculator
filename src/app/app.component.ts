074e2cd0dc1124c35b0725d4c98fc5061c165f67
Osama-Atwa <ashrafosama536@gmail.com>
2022-08-13 15:30:48 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vwn_calculator';
+  Vendor:number=0;
+  Media:number=0;
+  Corp:number=0;
+  TeamProject:number=0;
+  sum:number = 0;
+  Brex = 0;
+  Stripe = 0;
+  Amex:number = 0;
+  UR:number = 0;
+  LR:number = 0;
+
+  Show:number = 0;
+
+
+  OnBrex(){
+    this.sum = this.Vendor + this.Media + this.Corp + this.TeamProject;
+    this.Brex =this.sum * 0.3;
+    this.UR = this.sum * 1.1;
+    this.LR = this.sum * 1.2;
+    this.Show = 1;
+  }
+
+  OnStripe(){
+    this.sum = this.Vendor + this.Media + this.Corp + this.TeamProject;
+    this.Stripe = this.sum * 2.9;
+    this.UR = this.sum * 1.1;
+    this.LR = this.sum * 1.2;
+    this.Show = 2;
+  }
+
+  OnAmex(){
+    this.sum = this.Vendor + this.Media + this.Corp + this.TeamProject;
+    this.Amex =this.sum * 1.3
+    this.UR = this.sum * 1.1;
+    this.LR = this.sum * 1.2;
+    this.Show = 3;
+  }
+
+
 }
